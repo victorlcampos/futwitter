bc1578c0f86cf93c1d7809ff192b5e495c28e721
Victor Campos <victorcampos@ufrj.br>
2013-08-24 14:07:53 -0300

    limit news to 20

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,7 +2,7 @@ class TeamsController < ApplicationController
   def show
     @team = Team.find(params[:id])
     @match = @team.current_match
-    @news = @team.news
+    @news = @team.news.limit(20)
     @trusted_domains = TrustedDomain.pluck(:name)
   end
 end
